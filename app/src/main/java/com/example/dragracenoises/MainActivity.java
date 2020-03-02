f90a182bd315a236b551cbe19112eaaeea433a37
Jeff <garber34@gmail.com>
2020-03-01 22:53:59 -0500

    UI changes

--- app/src/main/java/com/example/dragracenoises/MainActivity.java
+++ app/src/main/java/com/example/dragracenoises/MainActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     //instantiate views
   ImageView satan;
@@ -39,29 +39,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         makeUp=findViewById(R.id.makeup);
         tone=findViewById(R.id.tone);
         shade=findViewById(R.id.shade);
-        satan.setOnClickListener(MainActivity.this);
-        showGirl.setOnClickListener(MainActivity.this);
-        successfully.setOnClickListener(MainActivity.this);
-        attacked.setOnClickListener(MainActivity.this);
-        bestFriend.setOnClickListener(this);
-        choices.setOnClickListener(this);
-        dontDoThat.setOnClickListener(this);
-        makeUp.setOnClickListener(this);
-        tone.setOnClickListener(this);
-        shade.setOnClickListener(this);
+        satan.setOnTouchListener(MainActivity.this);
+        showGirl.setOnTouchListener(MainActivity.this);
+        successfully.setOnTouchListener(MainActivity.this);
+        attacked.setOnTouchListener(MainActivity.this);
+        bestFriend.setOnTouchListener(this);
+        choices.setOnTouchListener(this);
+        dontDoThat.setOnTouchListener(this);
+        makeUp.setOnTouchListener(this);
+        tone.setOnTouchListener(this);
+        shade.setOnTouchListener(this);
 
     }
 
     @Override
-    public void onClick(View v) {
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        ImageView imageView = (ImageView) view;
+        switch (motionEvent.getAction()){
+        case MotionEvent.ACTION_DOWN:
+               imageView.setImageAlpha(128);
+            return true;
+            case MotionEvent.ACTION_UP:
+                imageView.setImageAlpha(255);
+                MediaPlayer mp;
+                int soundresid= getResources().getIdentifier((String)view.getTag(),"raw", getPackageName());
 
-        //uses tag of the source imageview to identify resource id of the associated sound clip
-        MediaPlayer mp;
-        int soundresid= getResources().getIdentifier((String)v.getTag(),"raw", getPackageName());
-
-        //plays sound clip using the resource id assigned above
-        mp = MediaPlayer.create(MainActivity.this, soundresid);
-       mp.start();
-
+                //plays sound clip using the resource id assigned above
+                mp = MediaPlayer.create(MainActivity.this, soundresid);
+                mp.start();
+                return true;
+    }
+        return false;
     }
 }
