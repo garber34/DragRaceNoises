5ad1448b887d64d993e31716d0d9f7ec9693850c
Jeff <garber34@gmail.com>
2020-03-08 15:17:22 -0400

    Untuck Player and UI improvements

--- app/src/main/java/com/example/dragracenoises/MainActivity.java
+++ app/src/main/java/com/example/dragracenoises/MainActivity.java
@@ -24,13 +24,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
   ImageView tone;
   ImageView shade;
 
+  //instantiate playlist UI and track elements
   Button bttnPlay;
   ImageView clipToSet;
   String index;
-
-    int[] playListResIDs = new int[5];
+    int[] playListResIDs = {0,0,0,0,0};
     private MediaPlayer playlistPlayer=null;
     int currentTrack=0;
+    ImageView currentPlaying;
 
 
     @Override
@@ -67,46 +68,84 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    //allows user to select playlist track and assign a clip
 public void selectSlot(View view){
-    if(clipToSet==null){
+        // checks whether the user is in selection mode, if not then fade the tapped ImageView and initialize that slot to be assigned
+        if(clipToSet==null){
         clipToSet = (ImageView) view;
         clipToSet.setBackgroundColor(Color.parseColor("#FFE503"));
         clipToSet.setImageAlpha(128);
         index = clipToSet.getTag().toString();
-        Toast.makeText(MainActivity.this, clipToSet.getTag().toString() + "", Toast.LENGTH_SHORT).show();
-    } else if(index.equals(view.getTag().toString())){
+        //Toast.makeText(MainActivity.this, clipToSet.getTag().toString() + "", Toast.LENGTH_SHORT).show();
+    }
+        //if the user clicks the slot they are already trying to assign, exit selection mode and return to playing clips one-off
+        else if(index.equals(view.getTag().toString())){
         clipToSet.setBackgroundColor(Color.parseColor("#FFFFFF"));
         clipToSet.setImageAlpha(256);
         clipToSet=null;
         index = "";
-    } else {
+    }
+        //if the user is topped on a different slot to assign, reset UI on original ImageView and switch to new ImageView
+        else {
         clipToSet.setBackgroundColor(Color.parseColor("#FFFFFF"));
         clipToSet.setImageAlpha(256);
          clipToSet = (ImageView) view;
         clipToSet.setBackgroundColor(Color.parseColor("#FFE503"));
         clipToSet.setImageAlpha(128);
         index = clipToSet.getTag().toString();
-        Toast.makeText(MainActivity.this, clipToSet.getTag().toString() + "", Toast.LENGTH_SHORT).show();
+       // Toast.makeText(MainActivity.this, clipToSet.getTag().toString() + "", Toast.LENGTH_SHORT).show();
     }
 }
-
+//play the playlist sequentially
    public void PlaySounds(View view) {
-           currentTrack=0;
+        //check that all 5 playlist slots are initialized, if not display error message
+       if( playListResIDs[0]==0 || playListResIDs[1]==0 ||playListResIDs[2]==0 ||playListResIDs[3]==0 ||playListResIDs[4]==0){
+           Toast.makeText(this, "Playlist not complete", Toast.LENGTH_SHORT).show();
+       }
+       //start playing sounds at first clip, recursively move to next clip on completion
+       else {
+           currentTrack = 0;
            playlistPlayer = MediaPlayer.create(MainActivity.this, playListResIDs[currentTrack]);
            playlistPlayer.setOnCompletionListener(this);
            playlistPlayer.start();
+
+           //fade ImageView of the first clip when playlist starts
+           currentPlaying = findViewById(R.id.playList1);
+           currentPlaying.setImageAlpha(128);
+       }
    }
 
+   //recursive function. when track completes, reset ImageView alpha, move to next track, fade that alpha, play new track
     public void onCompletion(MediaPlayer arg0) {
         arg0.release();
+        currentPlaying.setImageAlpha(255);
         if (currentTrack < playListResIDs.length-1) {
             currentTrack++;
+            switch (currentTrack){
+                case 1:
+                    currentPlaying=findViewById(R.id.playList2);
+                    currentPlaying.setImageAlpha(128);
+                    break;
+                case 2:
+                    currentPlaying=findViewById(R.id.playList3);
+                    currentPlaying.setImageAlpha(128);
+                    break;
+                case 3:
+                    currentPlaying=findViewById(R.id.playList4);
+                    currentPlaying.setImageAlpha(128);
+                    break;
+                case 4:
+                    currentPlaying=findViewById(R.id.playList5);
+                    currentPlaying.setImageAlpha(128);
+                    break;
+            }
             arg0 = MediaPlayer.create(getApplicationContext(), playListResIDs[currentTrack]);
             arg0.setOnCompletionListener(this);
             arg0.start();
         }
     }
 
+    //if in selection mode, assign clicked quote to selected slot, otherwise play it one time, fade image while playing
     @Override
     public void onClick(View view) {
                 switch(index) {
@@ -144,19 +183,48 @@ public void selectSlot(View view){
        }
        }
 
+       //handles the changing the  UI of the passed slot and overwriting the resource ID using the passed quote
        public void setPlayListSlot(View view, int slot){
            final ImageView imageView = (ImageView) view;
-        //set visual of playlist slot
+        //reset visual of playlist slot
            clipToSet.setImageAlpha(255);
            clipToSet.setBackgroundColor(Color.parseColor("#008000"));
            clipToSet.setImageDrawable(imageView.getDrawable());
 
            //set datasource for appropriate media player
            playListResIDs[slot]=getResources().getIdentifier((String) view.getTag(), "raw", getPackageName());
-           Toast.makeText(MainActivity.this, "Sound set", Toast.LENGTH_SHORT).show();
+
+           /*
            //deselect playlist slot
            clipToSet=null;
            index="";
+            */
+
+           //automatically select next slot, if at end of playlist exit selection mode
+           switch (index){
+               case "playList1":
+                   clipToSet=findViewById(R.id.playList2);
+                   selectSlot((clipToSet));
+                   break;
+                   case "playList2":
+                   clipToSet=findViewById(R.id.playList3);
+                   selectSlot((clipToSet));
+                   break;
+               case "playList3":
+                   clipToSet=findViewById(R.id.playList4);
+                   selectSlot((clipToSet));
+                   break;
+               case "playList4":
+                   clipToSet=findViewById(R.id.playList5);
+      selectSlot((clipToSet));
+                   break;
+               case "playList5":
+                   clipToSet=null;
+                   index="";
+                   break;
+
+
+           }
        }
     }
 
